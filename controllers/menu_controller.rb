9ea33c9016e62527ec4ba9a453e532f389d0e04a
Kara <kara.a.flaherty@gmail.com>
2017-06-29 12:57:43 -0600

    Added option to find an entry by entry number.

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,38 +10,43 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View entry by #"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
 
     case selection
     when 1 # view all entries
-        system "clear"
-        view_all_entries
-        main_menu
-      when 2 # create an entry
-        system "clear"
-        create_entry
-        main_menu
-      when 3 # search for an entry
-        system "clear"
-        search_entries
-        main_menu
-      when 4 # import entries from CSV
-        system "clear"
-        read_csv
-        main_menu
-      when 5 # exit
-        puts "Goodbye!"
-        exit(0)
-      else
-        system "clear"
-        puts "Sorry, that is not a valid input. Please try again."
-        main_menu
+      system "clear"
+      view_all_entries
+      main_menu
+    when 2 # view entry by #
+      system "clear"
+      view_entry_number
+      main_menu
+    when 3 # create an entry
+      system "clear"
+      create_entry
+      main_menu
+    when 4 # search for an entry
+      system "clear"
+      search_entries
+      main_menu
+    when 5 # import entries from CSV
+      system "clear"
+      read_csv
+      main_menu
+    when 6 # exit
+      puts "Goodbye!"
+      exit(0)
+    else
+      system "clear"
+      puts "Sorry, that is not a valid input. Please try again."
+      main_menu
     end
   end
 
@@ -49,11 +54,34 @@ class MenuController
     address_book.entries.each do |entry|
       system "clear"
       puts entry.to_s
-      entry_submenu(entry) # see def_entry_submenu below
+      entry_submenu(entry) # see entry_submenu below
     end
 
     system "clear"
     puts "End of entries"
+  end
+
+  def view_entry_number
+    puts "Enter '0' to exit to main menu"
+    print "Entry #: "
+    selection = gets.chomp.to_i
+
+    case selection
+    when 0
+      system "clear"
+      main_menu
+    when 1..address_book.entries.length
+      system "clear"
+      i = selection - 1
+      puts address_book.entries[i]
+      puts "---------------"
+      view_entry_number
+    else
+      system "clear"
+      puts "Sorry, that is not a valid entry number.\nThere are only #{address_book.entries.count} entries.\nPlease try again."
+      puts "---------------"
+      view_entry_number
+    end
   end
 
   def create_entry
@@ -87,16 +115,16 @@ class MenuController
     selection = gets.chomp
 
     case selection
-      when "n" # next entry
-      when "d" # delete entry
-      when "e" # edit entry
-      when "m" # return to main menu
-        system "clear"
-        main_menu
-      else
-        system "clear"
-        puts "#{selection} is not a valid input"
-        entry_submenu(entry)
+    when "n" # next entry
+    when "d" # delete entry
+    when "e" # edit entry
+    when "m" # return to main menu
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      entry_submenu(entry)
     end
   end
 end
